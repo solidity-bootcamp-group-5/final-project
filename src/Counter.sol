820c909e36125309c5866564385ac7223c4234a0
Riccardo Persiani <riccardo@ssvlabs.io>
2025-03-24 15:48:22 +0100

    feat: <- setup vault

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.28;
 
-contract Counter {
-    uint256 public number;
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract InvestmentVault is ERC4626 {
+    constructor(address underlying) ERC20("InvestmentVault", "IV") ERC4626(IERC20(underlying)) {}
+
+    function mint(address account, uint256 amount) external {
+        _mint(account, amount);
     }
 
-    function increment() public {
-        number++;
+    function burn(address account, uint256 amount) external {
+        _burn(account, amount);
     }
 }
