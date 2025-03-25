9da33f61eca7db76e661938d30700038799058bd
Riccardo Persiani <riccardo@ssvlabs.io>
2025-03-25 10:10:19 +0100

    test: <- aave supply completed

--- test/InvestmentVault.t.sol
+++ test/InvestmentVault.t.sol
@@ -21,7 +21,7 @@ contract InvestmnentVaultTest is Test {
     address usdcAddress = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address usdcWhale = 0x37305B1cD40574E4C5Ce33f8e8306Be057fD7341;
     address aavePool = 0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2;
-    address aUSDCv2 = 0xBcca60bB61934080951369a648Fb03DF4F96263C;
+    address aUSDC = 0x98C23E9d8f34FEFb1B7BD6a91B7FF122F4e16F5c;
 
     uint256 constant INITIAL_USER1_BALANCE_ETH = 20 ether;
     uint256 constant INITIAL_USER2_BALANCE_ETH = 10 ether;
@@ -43,7 +43,7 @@ contract InvestmnentVaultTest is Test {
         usdc = IERC20(usdcAddress);
 
         vm.startPrank(OWNER);
-        vault = new InvestmentVault(address(usdc), aavePool, aUSDCv2);
+        vault = new InvestmentVault(address(usdc), aavePool, aUSDC);
         vm.stopPrank();
 
         vm.label(address(vault), "InvestmentVault");
@@ -64,16 +64,18 @@ contract InvestmnentVaultTest is Test {
     function test_deposit() public {
         vm.selectFork(mainnetFork);
         vm.prank(usdcWhale);
-        usdc.transfer(USER1, 1000);
-        assertEq(usdc.balanceOf(USER1), 1000);
+        uint256 amount = 1000 * 10 ** 6;
+
+        usdc.transfer(USER1, amount);
+        assertEq(usdc.balanceOf(USER1), amount);
         vm.startPrank(USER1);
-        usdc.approve(address(vault), 1000);
+        usdc.approve(address(vault), amount);
         assertEq(vm.activeFork(), mainnetFork);
         assertEq(vault.balance(), 0);
-        vault.deposit(10, USER1);
-        assertEq(usdc.balanceOf(USER1), 990);
-        assertEq(vault.balanceOf(USER1), 10);
-        // assertEq(vault.balance(), 10);
+        vault.deposit(amount, USER1);
+        assertEq(usdc.balanceOf(USER1), 0);
+        assertEq(vault.balanceOf(USER1), amount);
+        assertEq(vault.balance(), amount);
         vm.stopPrank();
     }
 
