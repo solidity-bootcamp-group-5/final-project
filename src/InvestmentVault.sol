864603d5db9e6f5858076ab5ced211f0c6f993f4
José Garção <garcao.random@gmail.com>
2025-03-25 12:54:26 +0000

    add vault mint and withdraw functions

--- src/InvestmentVault.sol
+++ src/InvestmentVault.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.28;
 
+import "forge-std/console.sol";
+
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
@@ -39,6 +41,21 @@ contract InvestmentVault is ERC4626 {
         return shares;
     }
 
+    function mint(uint256 shares, address receiver) public override returns (uint256) {
+        uint256 maxShares = maxMint(receiver);
+        if (shares > maxShares) {
+            revert ERC4626ExceededMaxMint(receiver, shares, maxShares);
+        }
+
+        uint256 assets = previewMint(shares);
+        _deposit(_msgSender(), receiver, assets, shares);
+
+        IERC20(usdc).approve(address(aavePool), assets);
+        aavePool.supply(address(usdc), assets, address(this), 0);
+
+        return assets;
+    }
+
     function redeem(uint256 shares, address receiver, address owner) public override returns (uint256) {
         uint256 maxShares = maxRedeem(owner);
         if (shares > maxShares) {
@@ -52,6 +69,21 @@ contract InvestmentVault is ERC4626 {
         _withdraw(_msgSender(), receiver, owner, assets, shares);
 
         return assets;
+    }
+
+    function withdraw(uint256 assets, address receiver, address owner) public override returns (uint256) {
+        uint256 maxAssets = maxWithdraw(owner);
+        if (assets > maxAssets) {
+            revert ERC4626ExceededMaxWithdraw(owner, assets, maxAssets);
+        }
+
+        uint256 shares = previewWithdraw(assets);
+
+        aavePool.withdraw(address(usdc), assets, address(this));
+
+        _withdraw(_msgSender(), receiver, owner, assets, shares);
+
+        return shares;
     }
 
     function totalAssets() public view override returns (uint256) {
